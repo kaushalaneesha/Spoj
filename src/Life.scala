22596a2c48ca45cfc60c1d699782914f5449d842
Aneesha Kaushal <aneeshakaushal1992@gmail.com>
2016-02-08 16:03:02 +0530

    Taking inputs

---
+++ src/Life.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by akaushal on 2/8/2016.
+  */
+import scala.io.StdIn._;
+object Life extends App{
+      var x = readInt()
+      while(x!=42){
+        println(x)
+        x = readInt()
+      }
+}
